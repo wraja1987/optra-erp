b8655b58ca3c1010c84e1d80d2a15f06dca55639
Your Name <your-email@example.com>
2025-09-04 00:08:39 +0100

    test(mobile): polyfill setImmediate in jest setup

--- apps/mobile/jest.setup.js
+++ apps/mobile/jest.setup.js
@@ -4,5 +4,12 @@ jest.mock('@react-native-async-storage/async-storage', () => ({
   removeItem: jest.fn(async () => {}),
 }))
 
+// Polyfill setImmediate used by react-native-screens in tests
+if (typeof global.setImmediate === 'undefined') {
+  // eslint-disable-next-line no-undef
+  // @ts-ignore
+  global.setImmediate = (fn, ...args) => setTimeout(fn, 0, ...args)
+}
+
 
 
